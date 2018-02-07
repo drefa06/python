5010f64ad9e30b37e85a5234d64209d54f083575
drefa06 <drefa06@yahoo.fr>
2018-02-07 11:02:00 +0100

    Add NT solution, create an independant threading class over the select usage
    Signed-off-by: drefa06 <drefa06@yahoo.fr>

--- lib/inputNonBlocking.py
+++ lib/inputNonBlocking.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import threading
-import sys, time
+import sys, time, os
 import select
 import signal
 
 import pdb
+
+if os.name == 'nt':
+    import msvcrt
+else:
+    import select
+
 
 if (sys.version_info < (3, 0)):
     import Queue as queue
@@ -14,27 +19,60 @@ if (sys.version_info < (3, 0)):
 else:
     import queue
 
+
+class chkSysInput(threading.Thread):
+    def __init__(self,qRead,interrupt,timeout=1):
+        threading.Thread.__init__(self)
+        self.__qRead = qRead
+        self.__interrupt = interrupt
+        self.__timeout = timeout
+
+    def run(self):
+        while not self.__interrupt.isSet():
+            # tant que le locker est locked, controle sys.stdin
+            if os.name == 'nt':
+                while msvcrt.kbhit():
+                    char = msvcrt.getche()
+                    if char != '\r':
+                        self.buff += char
+                    else:
+                        self.__qRead.put(self.buff)
+                        self.buff = ""
+                        break
+
+            else:
+                read, _, _ = select.select([sys.stdin], [], [], self.__timeout)
+                if read:
+                    # quelquechose a lire, le placer dans la queue
+                    val = read[0].readline()
+
+                    self.__qRead.put(val)
+                    self.__qRead.join()
+                    time.sleep(0.1)
+                    self.__interrupt.set()
+
 class mngInput:
     """Cette classe permet de gerer un input non bloquant.
        input bloque le terminal
-       cet input laisse la main au programme principal toutes les secondes, il doit donc etre inclu dans une boucle de test.
+       cet input laisse la main au programme principal toutes les secondes, il doit donc etre inclu dans une boucle de tests.
        cet input peut etre interrompu
     """
     def __init__(self,prompt):
         """ __init__ de la classe
         :param prompt: le prompt a afficher avant de tester l'entree
         """
-
         self.q_read = queue.Queue()
-        self.interrupt = threading.Lock()
+        #self.interrupt = threading.Lock()
+        self.interruptEvent = threading.Event()
 
-        self.threadRead = threading.Thread(target=self.checkSysInput, args=(self.q_read,))
+        #self.threadRead = threading.Thread(target=self.checkSysInput, args=(self.q_read,))
+        self.threadRead = chkSysInput(self.q_read,self.interruptEvent)
         self.threadRead.daemon = True
-
-        signal.signal(signal.SIGINT, self.interruptInput)
 
         sys.stdout.write(prompt)
         sys.stdout.flush()
+
+        self.buff=""
 
     def interruptInput(self,*args, **kwargs):
         """ interruptInput: permet l'interruption de la thread definie par self.threadRead via le locker self.interrupt
@@ -46,7 +84,8 @@ class mngInput:
         if self.threadRead.is_alive():
             try:
                 """libere le locker"""
-                self.interrupt.release()
+                #self.interrupt.release()
+                self.interruptEvent.set()
             except threading.ThreadError as err:
                 if str(err) == 'release unlocked lock':
                     """cas particulier du locker unlocked"""
@@ -55,10 +94,9 @@ class mngInput:
                     """les autres cas d'erreurs doivent etre remonté"""
                     raise threading.ThreadError(str(err))
 
-            self.threadRead.join()
+            #self.threadRead.join()
 
-        sys.stdout.write("[interrupted]")
-        sys.stdout.flush()
+        return True
 
 
     def checkSysInput(self,q_read):
@@ -68,14 +106,23 @@ class mngInput:
         :return: RIEN
         """
         while self.interrupt.locked():
-            """tant que le locker est locked, controle sys.stdin"""
-            #time.sleep(0.1)
-
-            read, _, _ = select.select([sys.stdin], [], [], 1)
-            if read:
-                """quelquechose a lire, le placer dans la queue"""
-                val = read[0].readline()
-                self.q_read.put(val)
+            #tant que le locker est locked, controle sys.stdin
+            if os.name == 'nt':
+                while msvcrt.kbhit():
+                    char = msvcrt.getche()
+                    if char != '\r':
+                        self.buff += char
+                    else:
+                        self.q_read.put(self.buff)
+                        self.buff = ""
+                        break
+                    
+            else:
+                read, _, _ = select.select([sys.stdin], [], [], 1)
+                if read:
+                    #quelquechose a lire, le placer dans la queue
+                    val = read[0].readline()
+                    self.q_read.put(val)
 
 
     def getInput(self):
@@ -85,7 +132,8 @@ class mngInput:
         """
         text=None
         if not self.threadRead.is_alive():
-            self.interrupt.acquire()
+            self.interruptEvent.clear()
+            #self.interrupt.acquire()
 
             self.threadRead.start()
 
@@ -101,8 +149,8 @@ CIN = None
 """Locker sur les action sur la classe, indispensable si l'appel de Input et l'interruption ne sont pas sur la meme thread"""
 CINlock = threading.Lock()
 
-def inputNonBlocking(prompt):
-    """ inputNonBlocking: The fonction to use in your application
+def Input(prompt):
+    """ Input: c'est la fonction a importer
 
     :param prompt: Le prompt a ecrire avant la ligne a entrer par l'utilisateur
     :return: la ligne lue
@@ -113,14 +161,20 @@ def inputNonBlocking(prompt):
         CIN = mngInput(prompt)
 
     val = CIN.getInput()
-    CINlock.release()
+
     if val != None:
-        interruptInput()
+        CIN.interruptInput()
+        del CIN
+        CIN=None
+        #interruptInput()
+
+    CINlock.release()
+
     return val
 
 
 def interruptInput(*args, **kwargs):
-    """ interruptInput: The fonction that break the continuous input check
+    """
 
     :param args: utilisé par signal lors de l'interruption par CTRL+C
     :param kwargs:
@@ -133,8 +187,11 @@ def interruptInput(*args, **kwargs):
         del CIN
         CIN=None
 
+        sys.stdout.write('[interrupted]')
+        sys.stdout.flush()
 
     CINlock.release()
+
 
 """Control du signal CTRL+C"""
 signal.signal(signal.SIGINT, interruptInput)
@@ -142,46 +199,21 @@ signal.signal(signal.SIGINT, interruptInput)
 
 
 if __name__ == "__main__":
-    i=0
-    finish = False
-    while not finish:
-        i +=1
+    #Cette partie permet d'appeller une fonction de ce module en ligne de commande: python lib/ip.py <fonction> [<arg> [<arg>] ... ]
+    #c'est utilisé par les test pour le cas particulier de askIP pour simuler une entrée de commande par l'utilisateur
+    import sys,time
+    fct = locals()[sys.argv[1]]
 
-        val = Input("Enter Something: ")
-
-        if val != None:
-            print("You entered: {}".format(val))
-            interruptInput()
-            finish=True
-
-        else:
-            sys.stdout.write(".")
-            sys.stdout.flush()
+    if sys.argv[1] == 'Input':
+        while True:
             time.sleep(1)
+            if len(sys.argv) >2:
+                args = sys.argv[2:]
+                ret = fct(*args)
+            else:
+                ret = fct()
 
-        if i == 10:
-            interruptInput()
-            print("[interrupted]")
-            finish=True
+            if ret:
+                print('read: {}'.format(ret))
+                break
 
-    i=0
-    finish = False
-    while not finish:
-        i +=1
-
-        val = Input("Enter Something: ")
-
-        if val != None:
-            print("You entered: {}".format(val))
-            interruptInput()
-            finish=True
-
-        else:
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            time.sleep(1)
-
-        if i == 10:
-            interruptInput()
-            print("[interrupted]")
-            finish=True
