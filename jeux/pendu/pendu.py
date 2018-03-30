af6d9e652691f1ee7e96579f9c65b21025af35f1
drefa06 <drefa06@yahoo.fr>
2018-03-30 14:04:22 +0200

    Fix letter analysis
    Signed-off-by: drefa06 <drefa06@yahoo.fr>

--- jeux/pendu/pendu.py
+++ jeux/pendu/pendu.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import pdb
 import random
 import variable,fonctions
 
@@ -12,10 +13,10 @@ mot_a_trouver=variable.mots[random.randint(1,len(variable.mots))-1]
 mot_status=[{l:False} for l in mot_a_trouver]
 mot_tour=list("*"*len(mot_a_trouver))
 
-
-not_found=True
+pdb.set_trace()
+finish=False
 tour=1
-while not_found and tour <= variable.nb_tour:
+while not finish:
     lettre = fonctions.get_lettre()
 
     if lettre in mot_a_trouver:
@@ -25,18 +26,31 @@ while not_found and tour <= variable.nb_tour:
                 mot_tour[i]=lettre
     
     print "tour {0}: mot = {1}".format(tour,"".join(mot_tour))
+
     proposition = raw_input("Votre proposition ? ").lower()
     if proposition == mot_a_trouver:
-        not_found = False
-    elif proposition == "":
-        tour +=1
+        print("Felicitation {}, vous avez trouve le mot! ".format(name))
+        finish = True
 
-if not_found == False:
-    print("Felicitation {}, vous avez trouve le mot! ".format(name))
+    elif tour >= variable.nb_tour:
+        print("Desole {}, c'est perdu !".format(name))
+        print "Il fallait trouver le mot: {0}".format(mot_a_trouver)
+        finish = True
 
-elif tour > variable.nb_tour:
-    print("Desole {}, c'est perdu !".format(name))
-    print "Il fallait trouver le mot: {0}".format(mot_a_trouver)
+    else:
+        goodPlace=0
+        badPlace=0
+        for i,p in enumerate(proposition):
+            if i>=len(mot_a_trouver):
+                if p in mot_a_trouver:   badPlace+=1
+            else:
+                if p==mot_a_trouver[i]:  goodPlace+=1
+                elif p in mot_a_trouver: badPlace+=1
+
+        print("    {} lettres bien placees, {} lettres mal placees".format(goodPlace,badPlace))
+        
+        tour += 1
+
         
 print("Vous marquez {} points".format(variable.nb_tour-tour+1))
 scores[name]=scores[name]+variable.nb_tour-tour+1
